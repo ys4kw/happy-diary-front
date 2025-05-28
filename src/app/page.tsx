664fe9fa01b9ec635b352b6da1cbabe179ea6cc2
Yusuke Kikkawa <yusuke.kikkawa@gmail.com>
2025-05-28 12:29:09 +0900

    中央の『日記を書く』ボタンを茶色に変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+import Button from "@mui/material/Button";
+import styles from "./page.module.css";
+
 export default function Home() {
-  return null;
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <Button
+          variant="contained"
+          sx={{
+            fontSize: 24,
+            padding: "16px 48px",
+            borderRadius: 32,
+            backgroundColor: "#6b4f2a",
+            color: "#fff",
+            "&:hover": {
+              backgroundColor: "#4e3b1f",
+            },
+          }}
+        >
+          日記を書く
+        </Button>
+      </main>
+    </div>
+  );
 }
